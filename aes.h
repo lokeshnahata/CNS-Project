7a6ee107a175c92c20323add7598e62fbb2016de
Lokesh Nahata <47276583+lokeshnahata@users.noreply.github.com>
2021-06-24 11:10:51 +0530

    Create aes.h

---
+++ aes.h
@@ -0,0 +1,11 @@
+#ifndef AES_128_H
+#define AES_128_H
+#include <stdio.h>
+#include <stdint.h>
+#define AES_BLOCK_SIZE      16
+#define AES_ROUNDS          10  // 12, 14
+#define AES_ROUND_KEY_SIZE  176 // AES-128 has 10 rounds, and there is a AddRoundKey before first round. (10+1)x16=176.
+void aes_key_schedule_128(const uint8_t *key, uint8_t *roundkeys);
+void aes_encrypt_128(const uint8_t *roundkeys, const uint8_t *plaintext, uint8_t *ciphertext);
+void aes_decrypt_128(const uint8_t *roundkeys, const uint8_t *ciphertext, uint8_t *plaintext);
+#endif
